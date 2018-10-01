374ceb57c00c674f076510cf3610d1ba1d97f030
ellewilson96 <eleanorwilsonn@gmail.com>
2018-10-01 19:34:59 -0400

    Receive and insert course description via AJAX

--- course_registration/app/assets/javascripts/courses.js
+++ course_registration/app/assets/javascripts/courses.js
@@ -1,79 +1,20 @@
-$('.load_description').on('click', (e) => {
-    e.preventDefault()
-    $.get(`/courses`+.json)
+$(function () {
 
-    let courseID = 
-    let courseURL =
+  $('.course-details').on('click', (e) => {
 
-  $('.load_description').append(courseHtml)
+      //prevent default click acction
+    e.preventDefault();
 
+      // hide view description link
+    $('.course-details').hide();
 
-    function Course(course) {
-      this.id = course.id
-      this.title = course.section
-      this.name = course.name
-      this.description = course.description
-      this.user = course.user
-    }
+    // set course id to the course id of the clicked link
+    let courseID = $(".course-details").attr("data-id")
+      $.get("/courses/" + courseID + ".json", function(data) {
 
+      let courseHtml = data.description
+      $('#description-'+courseID).append("<h4>"+courseHtml+"</h4>");
 
-      Course.prototype.formatShow = function(){
-       let courseHtml = `
-       <h3>${this.description}</h3>
-       `
-        return courseHtml
-     }
-
-// $(( ) => {
-// bindClickHandlers()
-// })
-//
-// const bindClickHandlers = () => {
-//   $('.all_courses').on('click', (e) => {
-//     e.preventDefault()
-//     history.pushState(null, null, "courses")
-//     fetch(`/courses.json`)
-//     .then(res => res.json())
-//     .then(courses => {
-//       $('#contentBody').html('')
-//         courses.forEach((course) => {
-//           let newCourse = new Course(course)
-//
-//           let courseHtml = newCourse.formatIndex()
-//
-//           $('#contentBody').append(courseHtml)
-//         })
-//     })
-//   })
-//
-//   $(document).on('click', ".show_link", function(e) {
-//      e.preventDefault()
-//      $('#contentBody').html('')
-//
-//      let id = $(this).attr('data-id')
-//      fetch(`/courses/${id}.json`)
-//      .then(res => res.json())
-//      .then(course => {
-//        let newCourse = new Course(course)
-//
-//        let courseHtml = newCourse.formatShow()
-//        $('#contentBody').append(courseHtml)
-//      })
-//    })
-//  }
-
-//
-// Course.prototype.formatIndex = function(){
-//   let courseHtml = `
-//     <a href="/courses/${this.id}" data-id="class="show_link"><h1>${this.description}</h1></a>
-//   `
-//   return courseHtml
-// }
-//
-//
-// Course.prototype.formatShow = function(){
-//   let courseHtml = `
-//   <h3>${this.description}</h3>
-//   `
-//   return courseHtml
-// }
+    })
+  })
+})
